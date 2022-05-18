334a707908afb68bfa1f86c105d24f1a4ae7ed67
demian <demian@kcd.co.kr>
2022-05-19 00:49:33 +0900

    Update wallet-javascript

--- packages/wallet-javascript/index.ts
+++ packages/wallet-javascript/index.ts
@@ -1,14 +1,53 @@
+import Web3 from "web3";
+
+// TODO: check requirement and improve
 class Web3ETH {
-  connect() {
-    return { id: 1 };
+  private web3: Web3;
+
+  constructor({ url }: { url: string }) {
+    this.web3 = new Web3(url);
   }
 
-  getBalance() {
-    return null;
+  // TODO: generate random seed phrase and encrypt it with password. And change accounts.create() to accounts.privateKeyToAccount
+  signUp() {
+    return this.web3.eth.accounts.create();
   }
 
-  sendTransaction() {
-    return true;
+  // TODO: change to use password and seedPhrase to get privateKey
+  signIn(privateKey: string) {
+    return this.web3.eth.accounts.privateKeyToAccount(privateKey);
+  }
+
+  async getBalance(
+    address: string,
+    unit: keyof ReturnType<Web3["utils"]["unitMap"]> = "ether"
+  ) {
+    const balance = await this.web3.eth.getBalance(address);
+    return this.web3.utils.fromWei(balance, unit);
+  }
+
+  async signTransaction(
+    privateKey: string,
+    {
+      to,
+      value,
+      gas = 20000000000,
+    }: {
+      to: string;
+      value: string;
+      gas?: number;
+    }
+  ) {
+    const tx = await this.web3.eth.accounts.signTransaction(
+      {
+        to,
+        value,
+        gas,
+      },
+      privateKey
+    );
+
+    return tx;
   }
 }
 
